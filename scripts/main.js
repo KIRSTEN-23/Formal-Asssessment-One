6114a912e7c1f0e09fa7bb47d174aedb750b5935
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-08-02 12:20:10 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello");
+
+    // -----------------------------------------
+    // Home Page
+
+    // When the document loads, animate the hero image upwards
+    $(".hero-image").animate({top: '-=100px'});
+
+    // -----------------------------------------
+    // Browse Page
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+
+  }); 
